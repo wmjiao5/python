280461223e2f3140dbf9e4cd3df51da141da387a
wmjiao <3302532637@qq.com>
2023-04-17 16:39:29 +0800

    数据类型

---
+++ 1.python数据类型.py
@@ -0,0 +1,20 @@
+# 数据类型
+h = "爬虫"
+b = 1
+c = 1.0
+d = True
+e = 1+1j
+print(type(h))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(".................")
+# 数据类型的转换
+a = 1
+print(type(a))
+print(float(a))
+print(str(a))
+print(bool(a))
+print(complex(a))
+
